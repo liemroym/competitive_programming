742ed06dd5ea78ceee99feec300d323d1c80af68
liemroym <liemroymarcelino@gmail.com>
2022-05-22 14:09:32 +0700

    Removing some file

--- Codeforce/t_primes.cpp
+++ Codeforce/t_primes.cpp
@@ -7,6 +7,7 @@ using namespace std;
 #define pb(x) push_back(x)
 #define mp(x,y) make_pair(x,y)
 #define printV(vi) for (auto i : vi) cout << i << ' '
+#define input(a) for (int i = 0; i < a.size(); i++) cin >> a[i];
 #define INF 1e9					 
 #define gcd __gcd 				  // gcd(4, 6) = 2, gcd (30, 90) = 30, greatest common divisor
 #define PI 3.14159265
@@ -16,7 +17,22 @@ typedef pair<int, int> pii;
 typedef vector<int> vi;
 
 void solve() {
+	int n;
+	cin >> n;
 	
+	while(n--) {
+		int x;
+		cin >> x;
+		int count = 0;
+		for (int j = 2; j < x; j++) {
+			if (x % j == 0) {
+				count++;
+			} 
+			if (count > 1) break;
+		}	
+		
+		cout << (count == 1 ? "YES" : "NO") << endl;
+	}
 }
 
 int main() {
--- faster_template.cpp
+++ faster_template.cpp
@@ -1,3 +1,4 @@
+// Fenrir23
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -5,9 +6,9 @@ using namespace std;
 #define ull unsigned long long
 #define pb(x) push_back(x)
 #define mp(x,y) make_pair(x,y)
-#define printV(vi) fo(i, 0, vi.size()) cout << vi[i] << ' '
-#define fo(i, s, n) for (ll i = s; i < n; i++)
-#define INF 10e10+7
+#define printV(vi) for (auto i : vi) cout << i << ' '
+#define input(a) for (int i = 0; i < a.size(); i++) cin >> a[i];
+#define INF 1e9					 
 #define gcd __gcd 				  // gcd(4, 6) = 2, gcd (30, 90) = 30, greatest common divisor
 #define PI 3.14159265
 #define bpl __builtin_popcountll   // 4 --> 100 --> bpl = 1, 5 --> 101 --> bpl = 2, 6 --> 110 --> bpl = 2
